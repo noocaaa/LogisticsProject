d746f282cb2b0958eacc21356ce16645e9b908a5
noocaaa <noeliacarrascovilar@gmail.com>
2023-11-29 20:19:31 +0100

    Adding two methos for DriverController fixing #5

--- src/main/java/com/tsystems/logistics/service/DriverService.java
+++ src/main/java/com/tsystems/logistics/service/DriverService.java
@@ -106,5 +106,15 @@ public class DriverService {
         driver.setStatus(status);
         driverRepository.save(driver);
     }
+
+    public List<Driver> getAllDrivers() {
+        return driverRepository.findAll();
+    }
+
+    public Driver getDriverById(Integer id) {
+        return driverRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Driver not found with id: " + id));
+    }
+
 }
 
