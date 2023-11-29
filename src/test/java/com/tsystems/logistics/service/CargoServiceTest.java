853ee6df513b3a049f6cfa9b88910027bbc1ea30
noocaaa <noeliacarrascovilar@gmail.com>
2023-11-29 18:49:32 +0100

    Correcting an error on the testing file

--- src/test/java/com/tsystems/logistics/service/CargoServiceTest.java
+++ src/test/java/com/tsystems/logistics/service/CargoServiceTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
